866478e44e9afa03f8fdfb2ad0f9f2089c0c1e9a
LinkHpp <linkhpp4pokemon@gmail.com>
2024-02-19 12:53:16 +0100

    Initial commit

---
+++ Cliente.java
@@ -0,0 +1,33 @@
+// Clase Cliente
+import java.io.*;
+import java.net.*;
+
+public class Cliente {
+    public static void main(String[] args) throws IOException {
+        
+        try (Socket clienteSocket = new Socket("localhost", 55555);
+             PrintWriter out = new PrintWriter(clienteSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+            
+            String fromServer, fromUser;
+            
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Servidor: " + fromServer);
+                if (fromServer.equals("Tocado y hundido!")) {
+                    break;
+                }
+                
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Cliente: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.exit(1);
+        } catch (IOException e) {
+            System.exit(1);
+        }
+    }
+}
---
+++ Servidor.java
@@ -0,0 +1,63 @@
+// Clase Servidor
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(55555);
+        System.out.println("Esperando jugadores...");
+        
+        try {
+            Socket jugador1 = serverSocket.accept();
+            System.out.println("Jugador 1 conectado.");
+            Socket jugador2 = serverSocket.accept();
+            System.out.println("Jugador 2 conectado.");
+            
+            jugar(jugador1, jugador2);
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    private static void jugar(Socket jugador1, Socket jugador2) throws IOException {
+        BufferedReader in1 = new BufferedReader(new InputStreamReader(jugador1.getInputStream()));
+        PrintWriter out1 = new PrintWriter(jugador1.getOutputStream(), true);
+        BufferedReader in2 = new BufferedReader(new InputStreamReader(jugador2.getInputStream()));
+        PrintWriter out2 = new PrintWriter(jugador2.getOutputStream(), true);
+        
+        int contadorJugador1 = 0;
+        int contadorJugador2 = 0;
+        
+        String inputLine1, inputLine2;
+        
+        while (contadorJugador1 < 5 && contadorJugador2 < 5) {
+            // Turno del Jugador 1
+            out1.println("Tu turno.");
+            inputLine1 = in1.readLine();
+            System.out.println("Jugador 1: " + inputLine1);
+            if (inputLine1.equals("Tocado")) {
+                contadorJugador1++;
+                if (contadorJugador1 == 5) {
+                    out1.println("Tocado y hundido!");
+                    break;
+                }
+            }
+            
+            // Turno del Jugador 2
+            out2.println("Tu turno.");
+            inputLine2 = in2.readLine();
+            System.out.println("Jugador 2: " + inputLine2);
+            if (inputLine2.equals("Tocado")) {
+                contadorJugador2++;
+                if (contadorJugador2 == 5) {
+                    out2.println("Tocado y hundido!");
+                    break;
+                }
+            }
+        }
+        
+        // Cerrar conexiones
+        jugador1.close();
+        jugador2.close();
+    }
+}
